db37e93c37db86c15c30c9f82a7aad07f2ced490
thisisamondi <67878188+thisisamondi@users.noreply.github.com>
2023-01-24 21:50:29 +0100

    add styling to work page

--- components/CaseComponent.tsx
+++ components/CaseComponent.tsx
@@ -23,14 +23,19 @@ const CaseComponent = ({ title, description, artist, category }: any) => {
                     alt={artist.image.alt}
                   />
                 </div>
-                <p className="mt-1">{artist.name}</p>
+                <p className="mt-1 text-xl">{artist.name}</p>
                 {/* CATEGORY */}
-                <p>{category[0].title}</p>
+                {console.log(artist.category)}
+                {artist.category &&
+                  artist.category.map((category: any, index: any) => (
+                    <span key={index}>
+                      {(index ? ', ' : '') + category.title}
+                    </span>
+                  ))}
+
                 {/* COUNTRY */}
                 {artist.country.map((country: any, index: any) => (
-                  <span key={index}>
-                    {(index ? ', ' : '') + country.country}
-                  </span>
+                  <p key={index}>{(index ? ', ' : '') + country.country}</p>
                 ))}
               </div>
             ))}{' '}
